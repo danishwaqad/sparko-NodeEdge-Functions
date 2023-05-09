3138e6159273a03394f696189e575649f3686d50
Danish Waqad <danishwaqad@gmail.com>
2023-05-09 13:12:26 +0500

    changings for cloudflare

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const router = require('./router/route');
-const PORT = process.env.PORT || 3000;
-app.use('/', router);
-
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-});
